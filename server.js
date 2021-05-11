09ea3615d5509fe65446d1c41dcb1ae1ca95b0db
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-12 02:16:34 +1000

    create html routes

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const app = express();
+
 
 const PORT = process.env.PORT || 8080;
 
-const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+    // => HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+  
+    app.get('/index', (req, res) => {
+      res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
+  
+    app.get('/notes', (req, res) => {
+      res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+    });
+  
+    // If no matching route is found default to home
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
+
 
 // listening on port 8080
 app.listen(PORT, () => {
