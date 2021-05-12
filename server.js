db5844cac63217853574120865bb7ba5a5e13003
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-13 09:26:54 +1000

    db db.json path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-// const js = require('./assets/js');
+const { v4: uuidv4 } = require('uuid');
 
 
 const PORT = process.env.PORT || 8080;
@@ -12,21 +12,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let testArray = [
-  {
-  "title": "Test Title",
-  "text": "Test text"
-  },
-  {
-    "title": "Test Title",
-    "text": "Test text"
-    }
-]
 
 const notesDB = path.join(__dirname, 'db', 'db.json');
 
     // => HTML GET Requests
-    // Below code handles when users "visit" a page.
   
     app.get('/index', (req, res) => {
       res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -41,43 +30,17 @@ const notesDB = path.join(__dirname, 'db', 'db.json');
       res.sendFile(path.join(__dirname, 'public', 'index.html'));
     });
 
+
     app.get("/api/notes", (req, res) => {
-      fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+      fs.readFile(path.join(__dirname, '..db', 'db.json'), "utf8", (err, data) => {
           if (err) throw err;
           res.json(JSON.parse(data));
       });
   });
 
 
-  app.post("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
-        if (err) throw err;
-        const db = JSON.parse(data);
-        const newDB = [];
-
-        db.push(req.body);
-
-        for (let i = 0; i < db.length; i++)
-        {
-            const newNote = {
-                title: db[i].title,
-                text: db[i].text,
-                id: i
-            };
-
-            newDB.push(newNote);
-        }
-
-        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
-            if (err) throw err;
-            res.json(req.body);
-        });
-    });
-});
 
 
-
-    // app.get('/api/notes', (req, res) => res.json(testArray));
 
 // listening on port 8080
 app.listen(PORT, () => {
